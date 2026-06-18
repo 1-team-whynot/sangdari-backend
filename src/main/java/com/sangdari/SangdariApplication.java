3f63019c225c3e36ae598095fa692329fb83e87b
7une9ro <racoon121238@gmail.com>
2026-06-18 11:56:21 +0900

    260618 - 초기세팅

---
+++ src/main/java/com/sangdari/SangdariApplication.java
@@ -0,0 +1,13 @@
+package com.sangdari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangdariApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SangdariApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sangdari/SangdariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangdari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SangdariApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
